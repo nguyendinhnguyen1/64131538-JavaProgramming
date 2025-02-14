a0fdc9d785a0fde098fd7659dfebeac237a51ac0
nguyendinhnguyen1 <thanzbong115@gmail.com>
2025-02-14 22:46:21 +0700

    Ví dụ đầu tiên

---
+++ src/SayHelloApp.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * 
+ */
+public class SayHelloApp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Xin chào ");
+		System.out.println("Nguyễn Đình Nguyên ");
+	}
+
+}
