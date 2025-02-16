2ceb9349a061b4322c944b33e660874d6899e0fe
nguyendinhnguyen1 <thanzbong115@gmail.com>
2025-02-16 21:36:33 +0700

    đã làm xong bài Lab1

---
+++ Lab1/src/Bai3.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+
+/**
+ * 
+ */
+import java.util.Scanner;
+public class Bai3 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 	Scanner scanner = new Scanner(System.in);
+	        
+	        // Nhập cạnh của khối lập phương
+	        System.out.print("Nhập cạnh của khối lập phương: ");
+	        double canh = scanner.nextDouble();
+	        
+	        // Tính thể tích của khối lập phương
+	        double V = canh * canh * canh;
+	        
+	        // Xuất kết quả
+	        System.out.println("Thể tích của khối lập phương là: " + V);
+	        
+	        scanner.close();
+
+	}
+
+}
---
+++ Lab1/src/Bai4.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ */
+
+/**
+ * 
+ */
+import java.util.Scanner;
+public class Bai4 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 Scanner scanner = new Scanner(System.in);
+	        
+	        // Nhập các hệ số của phương trình bậc 2
+	        System.out.print("Nhập các hệ số a b c: ");
+	        double a = scanner.nextDouble();	        
+	        double b = scanner.nextDouble();	        
+	        double c = scanner.nextDouble();
+	        
+	        // Tính delta
+	        double delta = (b * b) - (4 * a * c);
+	        
+	     // Xuất căn delta
+	        if (delta > 0) {
+	            System.out.println("Căn delta: " + Math.sqrt(delta));
+	        } else if (delta == 0) {
+	            System.out.println("Căn delta: 0");
+	        } else {
+	            System.out.println("Căn delta là số phức: " + Math.sqrt(-delta) + "i");
+	        }
+       
+	        scanner.close();
+
+	}
+
+}
